0cc555a77c58b3e53d4b4c6fe61a14e4aba3e022
Meghana Bhimasani <m.bhimasani@gmail.com>
2019-03-01 00:32:15 -0500

    function xmltoJson, added styles

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,10 +10,54 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', function($scope, $http) {
-
-  $http.get('book/ab.json')
-       .then(function(response) {
-          $scope.contacts = response.data.AddressBook.Contact;
-        }
-      );
+  $http.get('book/ab.xml')
+   .then(function(response) {
+      console.log(response); // returns an object
+      var parser = new DOMParser();
+      var xml = parser.parseFromString(response.data, "text/xml"); // parse raw data to xml
+      var jsonObj = xmlToJson(xml); // convert xml to Json
+      $scope.contacts = jsonObj.AddressBook.Contact; // setting scope
+    }
+  );
 });
+
+
+function xmlToJson(xml) {
+
+  console.log(xml);
+
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -10,9 +10,52 @@ angular.module('myApp.view2', ['ngRoute'])
 }])
 
 .controller('View2Ctrl', function($scope, $http) {
-  $http.get('book/ab.json')
-       .then(function(response){
-          $scope.contacts = response.data.AddressBook.Contact;
-        }
-      );
+  $http.get('book/ab.xml')
+   .then(function(response){
+     var parser = new DOMParser();
+     var xml = parser.parseFromString(response.data, "text/xml");
+     var jsonObj = xmlToJson(xml);
+     $scope.contacts = jsonObj.AddressBook.Contact;
+    }
+  );
 });
+
+function xmlToJson(xml) {
+
+  console.log(xml);
+
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
